d1ebde9675aaaf6c658d69c17bb08b98340ebf21
Jonáš Bučina <99987632+silenyJonas@users.noreply.github.com>
2025-07-02 17:28:30 +0200

    footer ()42h

--- src/app/public/components/public-footer/public-footer.component.ts
+++ src/app/public/components/public-footer/public-footer.component.ts
@@ -8,6 +8,10 @@ import { Component, OnInit } from '@angular/core';
 export class PublicFooterComponent implements OnInit {
   currentYear: number;
 
+  in_link: string = 'assets/images/icons/in.png';
+  ig_link: string = 'assets/images/icons/ig.png';
+  fb_link: string = 'assets/images/icons/fb.png';
+
   constructor() {
     this.currentYear = new Date().getFullYear();
   }
