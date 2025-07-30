76e7fdcac23be6de83a316b53b3a56a3c6b2f876
Jonáš Bučina <99987632+silenyJonas@users.noreply.github.com>
2025-07-31 00:41:30 +0200

    asdasd

--- erp/src/app/app.config.ts
+++ erp/src/app/app.config.ts
@@ -1,8 +1,14 @@
+// src/app/app.config.ts
+
 import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { routes } from './app.routes';
 import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
+
+// Důležité importy pro HashLocationStrategy
+import { LocationStrategy, HashLocationStrategy } from '@angular/common';
+
+import { routes } from './app.routes'; // Import tvých rout
 import { AuthTokenInterceptor } from './core/interceptors/auth-token.interceptor';
 
 // DŮLEŽITÉ: Import pro registraci lokalizačních dat
@@ -17,6 +23,8 @@ export const appConfig: ApplicationConfig = {
     provideRouter(routes),
     provideHttpClient(withInterceptorsFromDi()),
     { provide: HTTP_INTERCEPTORS, useClass: AuthTokenInterceptor, multi: true },
+    // Zde přidáš HashLocationStrategy do pole 'providers'
+    { provide: LocationStrategy, useClass: HashLocationStrategy },
     // Volitelně můžete přidat PROVIDE_LOCALE, pokud chcete nastavit výchozí locale pro celou aplikaci
     // { provide: LOCALE_ID, useValue: 'cs-CZ' }
   ]
