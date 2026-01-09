8686e6db81539ccd35a90bb5d4644442a07d2749
silenyJonas <jonas.games@seznam.cz>
2026-01-09 15:41:22 +0100

    fb delted, jobs list text updated

---
+++ erp/src/app/admin/company-pages/knowledge-base/knowledge-base.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-knowledge-base',
+  imports: [],
+  templateUrl: './knowledge-base.component.html',
+  styleUrl: './knowledge-base.component.css',
+})
+export class KnowledgeBaseComponent {
+
+}
